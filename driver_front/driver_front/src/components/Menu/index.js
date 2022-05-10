dfcfebffd5df676a5732d5f595c4598ab38dde1e
Deividwb <bernardinodeivid@gmail.com>
2022-05-09 23:59:20 -0300

    import material

--- driver_front/driver_front/src/components/Menu/index.js
+++ driver_front/driver_front/src/components/Menu/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "../styles.css";
 import LogoImg from "../../assets/images/logo.png";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { NavLink } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from '@mui/icons-material/Search';
 
 const Menu = () => {
   return (
@@ -11,6 +15,58 @@ const Menu = () => {
             <span className="image">
               <img src={LogoImg} alt="logo" />
             </span>
+
+            <div className="text header-text">
+              <span className="name">Manager Driver</span>
+              <span className="profession">developer</span>
+            </div>
+          </div>
+          <div className="toggle">
+            <ArrowForwardIosIcon />
+          </div>
+
+          <div className="menu-bar">
+            <div className="menu">
+              <li className="nav-link">
+                <NavLink to={"/register_driver"}>
+                  <SearchIcon className="icon" />
+                </NavLink>
+                <input type="search" placeholder="Procurar..."></input>
+              </li>
+
+              <ul className="menu-links">
+                <li className="nav-link">
+                  <NavLink to={"/register_driver"}>
+                    <HomeIcon className="icon" />
+                  </NavLink>
+                  <span className="text nav-text">Dashboard</span>
+                </li>
+                <li className="nav-link">
+                  <NavLink to={"/register_driver"}>
+                    <HomeIcon className="icon" />
+                  </NavLink>
+                  <span className="text nav-text">Dashboard</span>
+                </li>
+                <li className="nav-link">
+                  <NavLink to={"/register_driver"}>
+                    <HomeIcon className="icon" />
+                  </NavLink>
+                  <span className="text nav-text">Dashboard</span>
+                </li>
+                <li className="nav-link">
+                  <NavLink to={"/register_driver"}>
+                    <HomeIcon className="icon" />
+                  </NavLink>
+                  <span className="text nav-text">Dashboard</span>
+                </li>
+                <li className="nav-link">
+                  <NavLink to={"/register_driver"}>
+                    <HomeIcon className="icon" />
+                  </NavLink>
+                  <span className="text nav-text">Dashboard</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </header>
       </nav>
---
+++ driver_front/driver_front/src/components/teste.js
@@ -0,0 +1,4 @@
+const teste = () => {
+  return <h1>Dentro teste</h1>;
+};
+export default teste;
